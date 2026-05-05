8c8f506be5a23f4b824fe5ec103c455d1648b0e5
Realdo-Justino <69261890+Realdo-Justino@users.noreply.github.com>
2026-05-05 09:54:37 -0300

    Removed The Explore Bar
    Also stole a bunch of code from the teacher, like the html and css

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,24 @@
-import {  StyleSheet, ScrollView, StatusBar } from 'react-native';
+import {  StyleSheet, ScrollView, StatusBar, Image, Text, View } from 'react-native';
 import {SafeAreaView } from 'react-native-safe-area-context';
+import { drivers } from "../../data/drivers.js";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.container} edges={['top']}>
       <ScrollView>
-
+        {
+          drivers.map((currentDriver) => (
+            <View style={styles.container}>
+              <Text style={styles.title}>Super Trunfo F1</Text>
+              <Image source={{ uri: currentDriver.image }} style={styles.image}></Image>
+              <Text style={styles.name}>{currentDriver.name}</Text>
+              <Text style={styles.country}>País: {currentDriver.country}</Text>
+              <Text style={styles.description}>{currentDriver.description}</Text>
+              <Text style={styles.starsTitle}>Estrelas:</Text>
+              <Text style={styles.stars}>{"⭐".repeat(currentDriver.stars)}</Text>
+            </View>
+          ))
+        }
       </ScrollView>
     </SafeAreaView>
   );
@@ -15,5 +28,43 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: StatusBar.currentHeight,
+    paddingBottom: 50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+   title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+    marginBottom: 16,
+  },
+  name: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 8
+  },
+  team: {
+    fontSize: 16
+  },
+  country: {
+    fontSize: 16,
+    marginBottom: 8
+  },
+  description: {
+    textAlign: 'center',
+    fontSize: 16,
+    marginBottom: 16
+  },
+  starsTitle: {
+    fontWeight: '600',
+    fontSize: 16
+  },
+  stars: {
+    fontSize: 20
   }
 });
